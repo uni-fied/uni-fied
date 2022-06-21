a200ef6db30506cca8a8c46a3d5618f7d445ebaa
uni-fied <devadiroot@gmail.com>
2022-06-21 17:14:59 +0700

    First push project basic for learning dart

---
+++ basic_dart/bin/hitung_persegi_panjang.dart
@@ -0,0 +1,6 @@
+import 'package:hitung_persegi_panjang/functions.dart' as basic_dart;
+
+void main(List<String> arguments) {
+  double result = basic_dart.hitungPersegiPanjang(5, 5);
+  print('Hasil penghitungan persegi panjang $result');
+}
---
+++ basic_dart/lib/functions.dart
@@ -0,0 +1,8 @@
+// Function Calculate
+int calculate() {
+  return 6 * 7;
+}
+// Function Calculate Persegi Panjang
+double hitungPersegiPanjang(double p, double l) {
+  return p * l;
+}
