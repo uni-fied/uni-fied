73de87596f984832bea8398c2e2699c0cd1a793b
uni-fied <devadiroot@gmail.com>
2022-06-22 11:08:18 +0700

    Menambahkan output result dari for loop bag. 1

--- dart-basic/01_for_loop.dart
+++ dart-basic/01_for_loop.dart
@@ -7,3 +7,23 @@ void main() {
     print("Looping ke-$i");
   }
 }
+
+/* 
+ * Output dari hasil eksekusi program diatas berikut : 
+ * 
+ * Looping ke-5
+ * Looping ke-6
+ * Looping ke-7
+ * Looping ke-8
+ * Looping ke-9
+ * Looping ke-10
+ * Looping ke-11
+ * Looping ke-12
+ * Looping ke-13
+ * Looping ke-14
+ * Looping ke-15
+ * Looping ke-16
+ * Looping ke-17
+ * Looping ke-18
+ * Looping ke-19
+ * */
