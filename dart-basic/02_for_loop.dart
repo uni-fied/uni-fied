3fa1a625ee367898aca04f0a45dca7ecaadac0d1
uni-fied <devadiroot@gmail.com>
2022-06-22 11:34:37 +0700

    For loop string, Bag. 2 For Loop

---
+++ dart-basic/02_for_loop.dart
@@ -0,0 +1,17 @@
+void main() {
+  var footballClub = ["Liverpool", "Man. United", "Man. City", "Celsea"];
+
+  for (String club in footballClub) {
+    int indexKe = footballClub.indexOf(club);
+    print("Football Club ke-$indexKe $club");
+  }
+}
+
+/* 
+ * Output dari hasil eksekusi program diatas berikut : 
+ * 
+ * Football Club ke-0 Liverpool
+ * Football Club ke-1 Man. United
+ * Football Club ke-2 Man. City
+ * Football Club ke-3 Celsea
+ * */
