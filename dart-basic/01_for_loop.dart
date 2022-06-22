eaeae470f9843219fe65e64b80e0db7a8a100a8a
uni-fied <devadiroot@gmail.com>
2022-06-22 11:04:11 +0700

    First up learn dart basic for looop bag. 1

---
+++ dart-basic/01_for_loop.dart
@@ -0,0 +1,9 @@
+void main() {
+  // Implementasi Null Safety
+  int? nilai_awal = 5;
+  int? nilai_akhir = 20;
+
+  for (int i = nilai_awal; i < nilai_akhir; i++) {
+    print("Looping ke-$i");
+  }
+}
